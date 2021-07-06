296fa45d42ea15505b866452106a4436db395f0e
talha700 <talhajaved700@gmail.com>
2021-07-06 05:41:44 -0400

    added page 404

--- frontend/src/components/PageNotFound.vue
+++ frontend/src/components/PageNotFound.vue
@@ -1,5 +1,9 @@
 <template>
-<div>
-    404 Not Found
-</div>
+  <v-flex fill-height md5 lg5 class="mx-auto mt-16" >
+      <div align="center">
+<v-img fill-height src="../assets/uurnikblack.png"></v-img>
+    <h2>(404) Page Not Found!</h2>
+    <v-btn large href="/" class="pageheading white--text mt-5" width="50px"><v-icon>mdi-home</v-icon></v-btn>
+      </div>
+  </v-flex>
 </template>
